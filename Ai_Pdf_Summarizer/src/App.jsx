3e6e34ab39f92f97e618302cc4f6961d0b9572db
Pranav <pranav123sharma123@gmail.com>
2026-05-26 12:22:31 +0530

    First Commit

---
+++ Ai_Pdf_Summarizer/src/App.jsx
@@ -0,0 +1,11 @@
+import UploadBox from "./components/UploadBox";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white">
+      <UploadBox />
+    </div>
+  );
+}
+
+export default App;
