0fc42e06f226aa1353dc82151c4456ab8065c623
Pranav <pranav123sharma123@gmail.com>
2026-06-03 16:06:39 +0530

    Feat: OCR routes

--- Ai_Pdf_Summarizer/server.js
+++ Ai_Pdf_Summarizer/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import { extractTextFromImage } from "./src/services/GeminiOCR.js";
+import { extractTextFromImage } from "./services/GeminiOCR.js";
 
 dotenv.config();
 
