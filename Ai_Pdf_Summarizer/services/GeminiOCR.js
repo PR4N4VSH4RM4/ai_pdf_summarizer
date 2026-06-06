67b7a5b47f61700c822d2bccef9278de31bb0dc3
Pranav <pranav123sharma123@gmail.com>
2026-06-06 17:16:38 +0530

    FIX: OCR image request

--- Ai_Pdf_Summarizer/services/GeminiOCR.js
+++ Ai_Pdf_Summarizer/services/GeminiOCR.js
@@ -1,14 +1,15 @@
 import fs from "fs";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(
-  process.env.GEMINI_API_KEY
-);
-
 export async function extractTextFromImage(
   imagePath,
   mimeType
 ) {
+  console.log("OCR KEY:", process.env.GEMINI_API_KEY);
+
+  const genAI = new GoogleGenerativeAI(
+    process.env.GEMINI_API_KEY
+  );
 
   const model = genAI.getGenerativeModel({
     model: "gemini-2.5-flash",
@@ -16,15 +17,47 @@ export async function extractTextFromImage(
 
   const imageData = fs.readFileSync(imagePath);
 
-  const result = await model.generateContent([
-    "Extract all text from this image exactly as written.",
-    {
-      inlineData: {
-        mimeType,
-        data: imageData.toString("base64"),
-      },
-    },
-  ]);
+  console.log("MIME:", mimeType);
+  console.log("IMAGE SIZE:", imageData.length);
 
-  return result.response.text();
-} 
+
+  for (let i = 0; i < 3; i++) {
+    try {
+      console.log("Before Gemini Call");
+
+      const result = await Promise.race([
+        model.generateContent([
+          "Extract all text from this image exactly as written.",
+          {
+            inlineData: {
+              mimeType,
+              data: imageData.toString("base64"),
+            },
+          },
+        ]),
+        new Promise((_, reject) =>
+          setTimeout(
+            () => reject(new Error("Gemini Timeout")),
+            30000
+          )
+        ),
+      ]);
+      
+      console.log("After Gemini Call");
+  
+      return result.response.text();
+  
+    } catch (error) {
+  
+      if (error.status === 503 && i < 2) {
+        console.log("Retrying Gemini...");
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        continue;
+      }
+  
+      throw error;
+    }
+  }
+
+
+}
